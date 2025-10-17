4799f14ac3cefe38d4dd9e9454f5383588f99b7a
herobrauni <braunnicolaj@gmail.com>
2025-10-17 08:35:03 +0200

    is this thing on?

--- Containerfile
+++ Containerfile
@@ -1,18 +1,23 @@
+# Build Arguments for NVIDIA control
+ARG NVIDIA_SUPPORT="false"
+ARG BASE_IMAGE_NAME="base"
+ARG IMAGE_TAG="latest"
+
+# Determine base image and image name based on NVIDIA support
+ARG BASE_IMAGE="ghcr.io/ublue-os/${BASE_IMAGE_NAME}-main"
+ARG IMAGE_NAME="${BASE_IMAGE_NAME}"
+
 # Allow build scripts to be referenced without being copied into the final image
 FROM scratch AS ctx
 COPY build_files /
 
-# Base Image
-FROM ghcr.io/ublue-os/bazzite:stable
+# Base Image - always start from base-main
+FROM ${BASE_IMAGE}:${IMAGE_TAG}
 
-## Other possible base images include:
-# FROM ghcr.io/ublue-os/bazzite:latest
-# FROM ghcr.io/ublue-os/bluefin-nvidia:stable
-# 
-# ... and so on, here are more base images
-# Universal Blue Images: https://github.com/orgs/ublue-os/packages
-# Fedora base image: quay.io/fedora/fedora-bootc:41
-# CentOS base images: quay.io/centos-bootc/centos-bootc:stream10
+ARG BASE_IMAGE_NAME="base"
+ARG IMAGE_TAG="latest"
+ARG IMAGE_NAME="${BASE_IMAGE_NAME}"
+ARG NVIDIA_SUPPORT="false"
 
 ### MODIFICATIONS
 ## make modifications desired in your image and install packages by modifying the build.sh script
@@ -22,8 +27,9 @@ RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
     --mount=type=cache,dst=/var/log \
     --mount=type=tmpfs,dst=/tmp \
-    /ctx/build.sh
-    
+    --mount=type=secret,id=GITHUB_TOKEN \
+    NVIDIA_SUPPORT="${NVIDIA_SUPPORT}" IMAGE_NAME="${IMAGE_NAME}" /ctx/build.sh
+
 ### LINTING
 ## Verify final image and contents are correct.
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,15 @@
 
 set -ouex pipefail
 
+# Get build arguments
+NVIDIA_SUPPORT="${NVIDIA_SUPPORT:-false}"
+IMAGE_NAME="${IMAGE_NAME:-base}"
+
+# If NVIDIA support is enabled, add nvidia to image name
+if [ "${NVIDIA_SUPPORT}" = "true" ]; then
+    IMAGE_NAME="${IMAGE_NAME}-nvidia"
+fi
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,7 +19,42 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
+
+# NVIDIA support - install if enabled
+if [ "${NVIDIA_SUPPORT}" = "true" ]; then
+    echo "Installing NVIDIA support..."
+
+    # Get current kernel version
+    KERNEL="$(rpm -q --queryformat='%{evr}.%{arch}' kernel-core)"
+    FEDORA_VERSION="$(rpm -E %fedora)"
+
+    # Fetch NVIDIA akmods
+    if skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia:main-"${FEDORA_VERSION}"-"${KERNEL}" dir:/tmp/akmods-rpms; then
+        NVIDIA_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-rpms/manifest.json | cut -d : -f 2)
+        tar -xvzf /tmp/akmods-rpms/"$NVIDIA_TARGZ" -C /tmp/
+        mv /tmp/rpms/* /tmp/akmods-rpms/
+
+        # Install NVIDIA drivers and tools
+        curl --retry 3 -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/main/main/build_files/nvidia-install.sh
+        chmod +x /tmp/nvidia-install.sh
+        IMAGE_NAME="${IMAGE_NAME}" /tmp/nvidia-install.sh
+
+        # Cleanup nouveau drivers and setup NVIDIA
+        rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+        ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
+
+        # Add kernel args for NVIDIA
+        tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<EOF
+kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1", "initcall_blacklist=simpledrm_platform_driver_init"]
+EOF
+
+        echo "NVIDIA support installed successfully"
+    else
+        echo "Failed to fetch NVIDIA akmods for kernel ${KERNEL}"
+        exit 1
+    fi
+fi
 
 # Use a COPR Example:
 #
